c07555cec1fddf169a35ec8a1c9c9462c9822ee3
Mihai Zofei <mihaizofei@yahoo.com>
2016-09-20 07:45:41 +0300

    start adding Redux flow

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
@@ -8,7 +10,11 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
+const store = configureStore();
+
 render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
